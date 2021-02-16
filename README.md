abd2627c945316b5720116a73c93c04556ad6230
TanithS <77208045+TanithS@users.noreply.github.com>
2021-02-16 10:32:23 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SwiftPlaygroundThree
+ Week 6, Class 11 - Tuesday, February 16, 2021: Intro to Arrays
---
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Part 3:
+
+var fruitNames = [String]()
+
+//Part 4:
+
+fruitNames = ["mango", "passionfruit", "guava"]
+
+//Part 5:
+
+print(fruitNames)
+
+//Part 6:
+
+print("I like to eat \(fruitNames)")
+
+//Part 7:
+
+for name in fruitNames {
+    print("I like to eat " + name + "!")
+}
+
+//Part 8:
+
+print(fruitNames[1])
